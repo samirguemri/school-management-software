b1ff62dbe6368d2baf568547492247bb1614b0ac
Samir <samir.guemri@gmx.fr>
2020-09-09 11:06:20 +0200

    Initial commit : Project Creation

---
+++ src/main/java/com/samir/sms/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.samir.sms;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SchoolManagementSoftwareApplication.class);
+    }
+
+}
---
+++ src/test/java/com/samir/sms/SchoolManagementSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samir.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
